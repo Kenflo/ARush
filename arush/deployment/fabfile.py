10f94b5d1719a2a6148fa2cf8b40ae6cc3523050
Börni <boerni@gmail.com>
2012-10-16 22:33:48 +0200

    [CHG] add service start/stop to deploy script

--- arush/deployment/fabfile.py
+++ arush/deployment/fabfile.py
@@ -18,13 +18,17 @@ def deploy_production():
     def _sudo(command, *args, **kwargs):
         sudo(command, user='www-data', *args, **kwargs)
 
+    # stop services
+    run('supervisorctl stop arush_collector')
+    run('supervisorctl stop arush_server')
+
     _sudo('cd %s; git reset --hard production && git pull' % (REMOTE_REPO_PATH))
 
     # install venv python modules
     _sudo('cd %s; source virtualenv/bin/activate; pip install -r requirements.txt' % (REMOTE_REPO_PATH))
 
     # replace websocket url
-    files.sed('%sarush/static/js/config.js' % (REMOTE_REPO_PATH), "http://localhost:9000/management/", WEBSOCKET_SERVER_URL)
+    files.sed('%sarush/static/js/config.js' % (REMOTE_REPO_PATH), "http://localhost:9000/ws", WEBSOCKET_SERVER_URL)
 
     # replace adn token
     files.sed('%sarush/settings.py' % (REMOTE_REPO_PATH), "^CFG_APPNET_TOKEN.*", 'CFG_APPNET_TOKEN = \"%s\"' % (APPNET_TOKEN))
@@ -32,3 +36,6 @@ def deploy_production():
     # replace mgmt url
     files.sed('%sarush/settings.py' % (REMOTE_REPO_PATH), "^CFG_TORNDO_MGMT_SERVER_URL.*", 'CFG_TORNDO_MGMT_SERVER_URL = \"%s\"' % (TORNDO_MGMT_SERVER_URL))
 
+    # start services
+    run('supervisorctl start arush_server')
+    run('supervisorctl start arush_collector')
